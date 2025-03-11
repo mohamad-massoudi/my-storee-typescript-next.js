a9089aa82e01ab9f210464c3025044b9e7bd46ce
Mohamad massoudi <mohamadmassoudi90@gmail.com>
2025-03-11 14:13:36 +0330

    Type Narrowing and Generics

--- my-store/app/page.tsx
+++ my-store/app/page.tsx
@@ -2,8 +2,8 @@ import ProductList from "./products";
 
 export default function Home() {
   return (
-    <div>
-      <h1>فروشگاه آنلاین کوچک</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600 mb-6">فروشگاه آنلاین کوچک</h1>
       <ProductList />
     </div>
   );
